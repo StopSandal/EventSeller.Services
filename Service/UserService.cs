4f677ca3e80dc7dc15aeffcf0d97c3612e3e8570
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-06-04 19:28:00 +0300

    add JWT token

---
+++ Service/UserService.cs
@@ -0,0 +1,59 @@
+﻿using EventSeller.DataLayer.Entities;
+using EventSeller.DataLayer.EntitiesViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.Services.Service
+{
+    public interface IUserService
+    {
+        Task<string> Login(LoginUserVM user);
+    }
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public UserService(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        public async Task<string> Login(LoginUserVM user)
+        {
+            /*
+            var LoginUser = .SingleOrDefault(x => x.UserName == user.UserName && x.Password == user.Password);
+
+            if (LoginUser == null)
+            {
+                return string.Empty;
+            }*/
+
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+            var jwtToken = new JwtSecurityToken(
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(30),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(
+                       Encoding.UTF8.GetBytes(_configuration["JWT:Secret"])
+                        ),
+                    SecurityAlgorithms.HmacSha256Signature)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
+    }
+}
