2aeac83fb8d7c840ebd0b9fa8b82c3fc8a917ece
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-07-01 12:00:00 +0300

    feat: added new DTO for bunch adding tickets

---
+++ Interfaces/Services/ITicketRegistrationService.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.Services.Interfaces.Services
+{
+    public interface ITicketRegistrationService
+    {
+        // all seats at hall with constraints( for circus with rows start and end )
+        Task<IEnumerable<Ticket>> AddTicketsForPlaceHallForAllSeatsAsync();
+        //directly for hall and count
+        Task<IEnumerable<Ticket>> AddTicketsForPlaceHallByCountAsync();
+
+    }
+}
---
+++ Service/TicketRegistrationService.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.Model;
+using EventSeller.Services.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSeller.Services.Service
+{
+    public class TicketRegistrationService : ITicketRegistrationService
+    {
+        public Task<IEnumerable<Ticket>> AddTicketsForPlaceHallByCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Ticket>> AddTicketsForPlaceHallForAllSeatsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
